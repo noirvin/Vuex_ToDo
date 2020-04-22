7af5b9e809ecda8d578fd8d0cdd4f4afa731c686
noirvin <vin.sp97@googlemail.com>
2020-04-22 16:39:54 -0700

    added some basic components

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import todos from './modules/todos'
+
+//Load vuex
+
+Vue.use(Vuex);
+
+
+//create store
+
+export default new Vuex.Store({
+    modules:{
+        todos
+    }
+}
+)
